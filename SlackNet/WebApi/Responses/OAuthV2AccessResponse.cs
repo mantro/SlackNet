a68838d1d8d8f7bb89c66d78ebc87351f553b381
Filip Stojanac <22033305+fstojanac@users.noreply.github.com>
2020-05-29 12:27:23 +0200

    [BUGFIX] Fix OAuthV2AccessResponse

--- SlackNet/WebApi/Responses/OAuthV2AccessResponse.cs
+++ SlackNet/WebApi/Responses/OAuthV2AccessResponse.cs
@@ -9,7 +9,7 @@ namespace SlackNet.WebApi
         public string BotUserID { get; set; }
         public string AppId { get; set; }
         public OAuthV2Team Team { get; set; }
-        public OAuthV2User User { get; set; }
+        public OAuthV2User AuthedUser { get; set; }
         public OAuthV2Enterprise Enterprise { get; set; }
 
     }
